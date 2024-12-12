8cc7b9f988796104df2547be8a68bb76d0d718eb
youngwoo <duddn6464@smartm2m.co.kr>
2024-12-12 05:07:41 +0000

    reference site(mangoboost) upload

---
+++ reference/www.mangoboost.io/_next/static/chunks/95b64a6e-ce777e145f8a571d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9866],{72567:function(t,n,u){u.d(n,{RYA:function(){return e}});var a=u(33141);function e(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M277.375 85v259.704l119.702-119.702L427 256 256 427 85 256l29.924-29.922 119.701 118.626V85h42.75z"}}]})(t)}}}]);
